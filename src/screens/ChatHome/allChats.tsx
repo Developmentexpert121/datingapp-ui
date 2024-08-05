7ccb3473d0e10a60b26fdc0c9df5b27a991f99b4
Devex Hub <54986861+Developmentexpert121@users.noreply.github.com>
2024-08-05 12:23:18 +0530

    Image in all chats

--- src/screens/ChatHome/allChats.tsx
+++ src/screens/ChatHome/allChats.tsx
@@ -10,6 +10,8 @@ import {
 import {ListItem, Avatar} from 'react-native-elements';
 import CommonBackbutton from '../../components/commonBackbutton/BackButton';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
 import {useNavigation} from '@react-navigation/native';
 import {useAppDispatch, useAppSelector} from '../../store/store';
 import {
@@ -142,6 +144,8 @@ const ChatSection = () => {
 
   //
   const renderItem = ({item}: {item: any}) => {
+    const isTextMessage = !item.chat.uri;
+
     return (
       <ListItem
         containerStyle={styles.listItemContainer}
@@ -177,7 +181,16 @@ const ChatSection = () => {
             style={{
               fontFamily: 'Sansation-Regular',
             }}>
-            {item.chat?.message}
+            {isTextMessage ? (
+              item.chat?.message
+            ) : (
+              <View style={{flexDirection: 'row'}}>
+                <Text>Photo</Text>
+                <View>
+                  <MaterialIcons name="photo" size={20} />
+                </View>
+              </View>
+            )}
           </Text>
         </View>
 
