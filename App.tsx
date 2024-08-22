34d11dc6ba8692fd6e4fcf8cc89ece371e9ff4c7
Devex Hub <54986861+Developmentexpert121@users.noreply.github.com>
2024-08-22 13:12:44 +0530

    Remove uwanted code

--- App.tsx
+++ App.tsx
@@ -1,43 +1,13 @@
-import React, {Fragment, useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import {SafeAreaProvider} from 'react-native-safe-area-context';
-import store, {
-  RootState,
-  useAppDispatch,
-  useAppSelector,
-} from './src/store/store';
 import messaging from '@react-native-firebase/messaging';
 import Root from './src/navigation/Root';
 import GlobalModal from './src/components/Modals/GlobalModal';
 import notifee, {AndroidImportance} from '@notifee/react-native';
-
-import Loader from './src/components/Loader/Loader';
-import io from 'socket.io-client';
-import {requestNotifications} from 'react-native-permissions';
-import {onlineUser} from './src/store/reducer/authSliceState';
 import {withIAPContext} from 'react-native-iap';
 import InternetModal from './src/components/Modals/InternetModal';
-// import PushController from './src/screens/Notification/PushController';
 import {navigationRef} from './src/utils/staticNavigation';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {ProfileData, videoCallToken} from './src/store/Auth/auth';
-import {
-  activityLoaderFinished,
-  activityLoaderStarted,
-  clientData,
-} from './src/store/Activity/activity';
-import {
-  CallingState,
-  StreamCall,
-  StreamVideo,
-  StreamVideoClient,
-  useCalls,
-} from '@stream-io/video-react-native-sdk';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import SplashScreen from 'react-native-splash-screen';
-import {Provider} from 'react-redux';
-import MyIncomingCallUI from './src/screens/ChatHome/myIncomingCallUI';
-import {PermissionsAndroid, Platform} from 'react-native';
 
 const App = () => {
   useEffect(() => {
